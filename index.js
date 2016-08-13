eef9cd0777092ec567de261fdaf70b55e0986e30
Christophe Hurpeau <christophe@hurpeau.com>
2016-08-13 18:27:58 +0200

    displayName

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
-var React = require('react');
+var createClass = require('react').createClass;
 var shallowEqual = require('fbjs/lib/shallowEqual');
 
 module.exports = function createPureStatelessComponent(statelessComponent) {
   if (typeof statelessComponent === 'function') {
     statelessComponent = {
+      displayName: statelessComponent.name,
       propTypes: statelessComponent.propTypes,
       contextTypes: statelessComponent.contextTypes,
       render: statelessComponent,
     };
   }
 
-  return React.createClass({
+  const displayName = statelessComponent.displayName || statelessComponent.name;
+
+  if (process.env.NODE_ENV !== 'production') {
+      if (!displayName) {
+        throw new Error('Invalid displayName');
+      }
+  }
+
+  return createClass({
+    displayName: displayName,
     propTypes: statelessComponent.propTypes,
     contextTypes: statelessComponent.contextTypes,
 
